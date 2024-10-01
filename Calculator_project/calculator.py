b8fff69a63a889cce77c0820b1957a53517c2abc
xavdon <xavdonp.72@gmail.com>
2024-10-02 00:35:27 +0530

    new project

---
+++ Calculator_project/calculator.py
@@ -0,0 +1,15 @@
+
+def add(x,y):
+    return x+y #define addition as a function
+
+def sub(x,y):
+    return x-y #define substraction as a function
+
+def multi(x,y):
+    return x * y #define multiplication as a function
+
+def div(x,y):
+    if y==0:
+        print('Division by zero is not possible')
+    else:
+        return x/y #define division as a function
